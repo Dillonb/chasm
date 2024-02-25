5b4de58b33abf88d936cc0d2bd2adc9a65423c44
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-02-24 20:01:15 -0800

    add annotations to capstone wrapper to fix GC

--- test/capstone_wrapper.c
+++ test/capstone_wrapper.c
@@ -2,14 +2,16 @@
 #include <caml/mlvalues.h>
 #include <caml/alloc.h>
 #include <caml/fail.h>
+#include <caml/memory.h>
 
 csh handle = 0;
 
-value disassemble(value code, value length) {
+CAMLprim value disassemble(value code, value length) {
+    CAMLparam2(code, length);
+
     unsigned char* code_ptr = Bytes_val(code);
     size_t code_size = Int_val(length);
 
-    caml_alloc_string(12);
     if (handle == 0) {
         if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
             caml_failwith("Failed to initialize Capstone");
@@ -23,9 +25,10 @@ value disassemble(value code, value length) {
         ssize_t buflen = snprintf(NULL, 0, "%s %s", insn[0].mnemonic, insn[0].op_str) + 1;
         char* buf = malloc(buflen);
         snprintf(buf, buflen, "%s %s", insn[0].mnemonic, insn[0].op_str);
-        value result = caml_copy_string(buf);
+        CAMLlocal1(result);
+        result = caml_copy_string(buf);
         free(buf);
-        return result;
+        CAMLreturn(result);
     } else {
         caml_failwith("Disassembling multiple instructions not supported");
     }
